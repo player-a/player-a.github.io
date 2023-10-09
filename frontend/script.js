3af3cba9c595ac6768b45c63cc910225a36e8ae6
player-a <ace.eldemir@gmail.com>
2023-10-09 12:58:55 -0500

    Complex Calculator App v1.2 - rewrote script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,35 @@
 async function performCalculation() {
+    // Fetch values from the form
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
     const operation = document.getElementById("operation").value;
 
-    const response = await fetch('https://my-complex-calculator-a3ecab1056de.herokuapp.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            num1: num1,
-            num2: num2,
-            operation: operation
-        })
-    });
+    try {
+        // Send a POST request to the /calculate endpoint
+        const response = await fetch('/calculate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                num1: num1,
+                num2: num2,
+                operation: operation
+            })
+        });
 
-    const data = await response.json();
-    if (data.error) {
-        document.getElementById("result").innerText = "Error: " + data.error;
-    } else {
-        document.getElementById("result").innerText = "Result: " + data.result;
+        // Parse the response JSON
+        const data = await response.json();
+
+        // Handle the response
+        if (data.error) {
+            document.getElementById("result").innerText = "Error: " + data.error;
+        } else {
+            document.getElementById("result").innerText = "Result: " + data.result;
+        }
+
+    } catch (error) {
+        // Handle any other errors (e.g., network issues)
+        document.getElementById("result").innerText = "Error: " + error.toString();
     }
 }
